cc545d4f5531b7ed9d5677f1574f4c1bb756f530
Colin Bauer <129115657+cbauer14@users.noreply.github.com>
2023-12-11 20:20:13 -0700

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 
 const API_BASE_URL =
-  process.env.REACT_APP_API_URL || "http://localhost:5000";
+  process.env.REACT_APP_API_URL || "https://backend-2yc1.onrender.com";
 
 /**
  * Defines the default headers for these functions to work with `json-server`
